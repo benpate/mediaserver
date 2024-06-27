eed7c34bf46120e6fb4c019f42e082ab3e3548d0
Ben Pate <ben@pate.org>
2024-06-26 19:52:47 -0600

    Adding obsessive logging
    Feeling cute, might delete later

--- mediaserver.go
+++ mediaserver.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 
 	"github.com/benpate/derp"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/afero"
 )
 
@@ -27,13 +28,16 @@ func New(original afero.Fs, cache afero.Fs) MediaServer {
 // Get locates the file, processes it if necessary, and returns it to the caller.
 func (ms MediaServer) Get(filespec FileSpec, destination io.Writer) error {
 
+	log.Trace().Str("filename", filespec.Filename).Msg("mediaserver.Get")
+
 	// If the file exists in the cache, then we're in luck :)
 	if cached, err := ms.cache.Open(filespec.CachePath()); err == nil {
+		log.Trace().Msg("mediaserver.Get - Found cached file")
 
 		defer cached.Close()
 
 		if _, err := io.Copy(destination, cached); err != nil {
-			return derp.Wrap(err, "mediaserver.Get", "Error copying cached file to destination", filespec)
+			return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error copying cached file to destination", filespec))
 		}
 
 		return nil
@@ -43,7 +47,7 @@ func (ms MediaServer) Get(filespec FileSpec, destination io.Writer) error {
 	original, err := ms.original.Open(filespec.Filename)
 
 	if err != nil {
-		return derp.NewNotFoundError("mediaserver.Get", "Cannot find original file", filespec, err)
+		return derp.ReportAndReturn(derp.NewNotFoundError("mediaserver.Get", "Cannot find original file", filespec, err))
 	}
 
 	defer original.Close()
@@ -52,19 +56,20 @@ func (ms MediaServer) Get(filespec FileSpec, destination io.Writer) error {
 	processedImage, err := ms.Process(original, filespec)
 
 	if err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error processing original file", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error processing original file", filespec))
 	}
 
 	// Guarantee that a cache folder exists for this file
 	exists, err := afero.DirExists(ms.cache, filespec.CacheDir())
 
 	if err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error locating directory for cached file", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error locating directory for cached file", filespec))
 	}
 
 	if !exists {
+		log.Trace().Msg("mediaserver.Get - Creating cache folder")
 		if err := ms.cache.Mkdir(filespec.CacheDir(), 0777); err != nil {
-			return derp.Wrap(err, "mediaserver.Get", "Error creating directory for cached file", filespec)
+			return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error creating directory for cached file", filespec))
 		}
 	}
 
@@ -72,14 +77,14 @@ func (ms MediaServer) Get(filespec FileSpec, destination io.Writer) error {
 	cacheWriter, err := ms.cache.Create(filespec.CachePath())
 
 	if err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error creating file in mediaserver cache", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error creating file in mediaserver cache", filespec))
 	}
 
 	defer cacheWriter.Close()
 
 	// Write the processed file into the cache (and copy to destination at the same time)
 	if _, err := io.Copy(cacheWriter, processedImage); err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error writing processed file to cache", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error writing processed file to cache", filespec))
 	}
 
 	// Close writer here so that we can re-read the file.
@@ -89,15 +94,17 @@ func (ms MediaServer) Get(filespec FileSpec, destination io.Writer) error {
 	cacheReader, err := ms.cache.Open(filespec.CachePath())
 
 	if err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error just-generated file", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error just-generated file", filespec))
 	}
 
 	defer cacheReader.Close()
 
 	// Write the just-cached file to the destination.
 	if _, err := io.Copy(destination, cacheReader); err != nil {
-		return derp.Wrap(err, "mediaserver.Get", "Error copying cached file to destination", filespec)
+		return derp.ReportAndReturn(derp.Wrap(err, "mediaserver.Get", "Error copying cached file to destination", filespec))
 	}
+
+	log.Trace().Msg("mediaserver.Get - Success")
 
 	// Success!!?!
 	return nil
