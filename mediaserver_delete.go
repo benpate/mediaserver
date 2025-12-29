3150f11f97ea4ff294700dd32aa5dd5e46436c0b
Ben Pate <ben@pate.org>
2025-12-29 10:11:05 -0700

    Error messages

--- mediaserver_delete.go
+++ mediaserver_delete.go
@@ -6,11 +6,11 @@ import "github.com/benpate/derp"
 func (ms MediaServer) Delete(filename string) error {
 
 	if err := ms.original.Remove(filename); err != nil {
-		return derp.Wrap(err, "mediaserver.Delete", "Error removing media file in 'original' filesystem", filename)
+		return derp.Wrap(err, "mediaserver.Delete", "Unable to remove media file in 'original' filesystem", filename)
 	}
 
 	if err := ms.processed.RemoveAll(filename); err != nil {
-		return derp.Wrap(err, "mediaserver.Delete", "Error removing media files in 'cache' filesystem", filename)
+		return derp.Wrap(err, "mediaserver.Delete", "Unable to remove media files in 'cache' filesystem", filename)
 	}
 
 	return nil
