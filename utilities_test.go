2bebe1f4cf3af5bd799ebaf7386c3142902df279
Ben Pate <ben@pate.org>
2026-06-16 09:18:37 -0600

    Remove test case
    getCoverPhoto is no longer used.

--- utilities_test.go
+++ utilities_test.go
@@ -5,17 +5,7 @@ package mediaserver
 import (
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
-
-func TestCoverPhoto(t *testing.T) {
-
-	filename, err := getCoverPhoto("http://localhost/6692c69bfe80a9aacf125b0d/attachments/6723b7b74aa88ca07dc8614e")
-
-	require.Nil(t, err)
-	t.Log(filename)
-}
 
 func TestTempDir(t *testing.T) {
 	t.Log(os.TempDir())
